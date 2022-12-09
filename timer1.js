898d29c6a0a409bedea83c76de031a5fed6bc13d
Victor Ha <victorha88@gmail.com>
2022-12-08 19:17:09 -0500

    Create Function for timer1.js

--- timer1.js
+++ timer1.js
@@ -1,16 +1,22 @@
-const args = process.argv.slice(2);
+const timer = function () {
+  const args = process.argv.slice(2);
 
-if (!args.length) {
-  return;
-};
-
-for (const num of args) {
-  if (num < 0 || isNaN(num)) {
-    continue;
+  if (!args.length) {
+    return;
   }
-  let timer = 0;
-  const timeInSeconds = num * 1000;
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, timer += timeInSeconds);
+
+  for (const num of args) {
+    if (num < 0 || isNaN(num)) {
+      continue;
+    }
+    let timer = 0;
+    const timeInSeconds = num * 1000;
+    setTimeout(() => {
+      process.stdout.write("\x07");
+    }, (timer += timeInSeconds));
+  }
 };
+
+timer();
+
+module.exports = { timer };
